aef6830378adc4c24aa6c573f323348e3237a486
Aso2001142 <83046954+Aso2001142@users.noreply.github.com>
2021-11-18 19:53:51 +0900

    Update reigai.py

--- python/reigai.py
+++ python/reigai.py
@@ -20,8 +20,20 @@ try:
 except ZeroDivisionError as e:
     print('0では割り算できません')
     #print(traceback.format_exc())   #例外情報の取得
-    sys.stderr.write(traceback.format_exc())    #
+    sys.stderr.write(traceback.format_exc())    #標準エラー出力
 finally:
     print(2)
     
-    
+#未定義変数
+print(1)
+try:
+    number = 1
+    answer = 100 / number
+    print(answer2)
+except NameError as e:
+    print("未定義の変数を呼び出しています")
+    print(e)
+finally:
+    print(2)
+
+#
