380061c91a663bb7310145efa8a79e5642265c19
Aso2001142 <83046954+Aso2001142@users.noreply.github.com>
2021-11-21 17:58:05 +0900

    Update reigai2.py

--- python/reigai2.py
+++ python/reigai2.py
@@ -1,1 +1,13 @@
+##raise
+
+print(1)
+try:
+    print(2)
+    raise Exception("意図的な例外") //エラーメッセージ
+    print(3)
+except Exception as e:
+    print("予期せぬエラーが発生しました")
+    print(e)
+finally:
+    print(4)
 
